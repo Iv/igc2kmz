2468a7a763ee28891ccb3bb30a8fc0ab7610391d
Ivan Kravchenko <iv.kravchenko@gmail.com>
2017-05-12 08:18:45 +0300

    Sceleton for free flight through up to 3 points optimisation.

--- igc2kmz/optimisation.py
+++ igc2kmz/optimisation.py
@@ -79,22 +79,57 @@ class OptimisationDeterministic(BaseOptimisation):
                         finish = c2
 
         return distance, start, finish
+    
+    def find_max_free_flight(self):
+        distance = 0
+        start = p1 = p2 = p3 = finish = None
+        n = len(self.coords)
+        for i, c1 in enumerate(self.coords):
+            for j in range(i, n):
+                for k in range(j,n):
+                    for l in range(k, n):
+                        for m in range(l, n):
+                            c2 = self.coords[j]
+                            c3 = self.coords[k]
+                            c4 = self.coords[l]
+                            c5 = self.coords[m]
 
+                            d = c1.distance_to(c2) + c2.distance_to(c3) \
+                                + c3.distance_to(c4) + c4.distance_to(c5)
+
+                            if d > distance:
+                                distance = d
+                                start = c1
+                                p1 = c2
+                                p2 = c3
+                                p3 = c4
+                                finish = c5
+
+        return distance, start, p1, p2, p3, finish
+    
 
 class Optimisation(BaseOptimisation):
 
     def find_max_distance(self):
         sets = []
+        
+        max_distance = 0
         for i, s1 in enumerate(self.sets):
-            if i<len(self.sets):
+            if i < len(self.sets):
                 for j, s2 in enumerate(self.sets[i+1:]):
-                    sets.append((s1.center.distance_to(s2.center), s1, s2))
+                    d = s1.center.distance_to(s2.center)
+                    if max_distance < d:
+                        max_distance = d
+                    
+                    if abs(max_distance - d) < 4*SET_RADIUS:
+                        sets.append((d, s1, s2))
+                        
 
         max_distance = 0
         for s in sets:
             if max_distance < s[0]:
                 max_distance = s[0]
-                
+
         sets = filter(lambda item: abs(max_distance - item[0]) < 4*SET_RADIUS, sets)
 
         coords = []
@@ -109,5 +144,64 @@ class Optimisation(BaseOptimisation):
                       distance = d
                       start = c1
                       finish = c2
-            
+
         return distance, start, finish
+
+    def find_max_free_flight(self):
+        distance = 0
+        start = p1 = p2 = p3 = finish = None
+        n = len(self.coords)
+
+        sets = []
+        ns = len(self.sets)
+        max_distance = 0
+        for i, s1 in enumerate(self.sets):
+            for j in range(i, ns):
+                for k in range(j, ns):
+                    for l in range(k, ns):
+                        for m in range(l, ns):
+                            s2 = self.sets[j]
+                            s3 = self.sets[k]
+                            s4 = self.sets[l]
+                            s5 = self.sets[m]
+                            d = (
+                                s1.center.distance_to(s2.center) +
+                                s2.center.distance_to(s3.center) +
+                                s3.center.distance_to(s4.center) +
+                                s4.center.distance_to(s5.center)
+                            )
+
+                            if max_distance < d:
+                                max_distance = d
+
+                            if abs(max_distance - d) < 18 * SET_RADIUS:
+                                sets.append((d, s1, s2, s3, s4, s5))
+
+        max_distance = 0
+        for s in sets:
+            if max_distance < s[0]:
+                max_distance = s[0]
+
+        sets = filter(lambda item: abs(max_distance - item[0]) < 18 * SET_RADIUS, sets)
+        
+        distance = 0
+        start = p1 = p2 = p3 = finish = None
+        
+        for s in sets:
+            for c1 in s[1].coords:
+                for c2 in s[2].coords:
+                    for c3 in s[3].coords:
+                        for c4 in s[4].coords:
+                            for c5 in s[5].coords:
+                                d = (
+                                    c1.distance_to(c2) +
+                                    c2.distance_to(c3) +
+                                    c3.distance_to(c4) +
+                                    c4.distance_to(c5)
+                                )
+
+                                if d > distance:
+                                    distance = d
+                                    start, p1, p2, p3, finish = c1, c2, c3, c4
+
+        return start, p1, p2, p3, finish
---
+++ utils/optimisation_examples.py
@@ -0,0 +1,27 @@
+import sys
+import os
+
+BASE_PATH = os.path.join(os.path.dirname(__file__), '..')
+sys.path.insert(0, BASE_PATH)
+
+EXAMPLES_DIR = os.path.join(BASE_PATH, 'examples_jonatan')
+
+from igc2kmz.optimisation import OptimisationDeterministic, Optimisation
+from igc2kmz.igc import IGC
+
+Opt = Optimisation
+
+def main(argv):
+    with open(os.path.join(EXAMPLES_DIR, '118_1.igc')) as f:
+        igc = IGC(f)
+        track = igc.track()
+        opt = Opt(track)
+
+        d, start, finish = opt.find_max_distance()
+        print ("Max distance: %0.2f" % d)
+
+        d, start, p1, p2 , p3, finish = opt.find_max_free_flight()
+        print ("Max free flight through up to tree points: %0.2f" % d)
+        
+if __name__ == "__main__":
+    main(sys.argv)
